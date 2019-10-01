e50bfe4b5d26fc729dffb4e8b89eccd98313da55
Benedikt Geier <benedikt1geier@web.de>
2019-10-01 14:01:25 +0200

    feat: Minor extensions and fixes

--- get_params.R
+++ get_params.R
@@ -201,7 +201,7 @@ iterate_min_max_vals("shape1", all_params, fam, cur_val=0, step_sizes = c(1, 0.5
 ### Main function for generating the info for each of the params ------------------------------------------------------
 
 get_param_ranges <- function(all_params, fam) {
-  lower <- upper <- accepts_float <- rep(0, length(all_params))
+  lower <- upper <- accepts_float <- rep(NA, length(all_params))
   names(lower) <- names(upper) <- names(accepts_float) <- names(all_params)
   
   for (param in names(all_params)){
@@ -235,13 +235,13 @@ get_param_ranges <- function(all_params, fam) {
     lower[param] <- min_val
     upper[param] <- max_val
     
-    ### TODO:
+    # 2) Check if the parameter accepts floats or only integers
     n <- 10
     testsequence <- runif(n, min_val, max_val)
     testsequence <- unique(c(testsequence, trunc(testsequence)))
     num_tests <- length(testsequence)
 
-    is_integer <- is.integer(testsequence)
+    is_integer <- testsequence%%1 == 0
     num_integer <- sum(is_integer)
 
     testoutcome <- check_values_for_param(param, all_params, fam, testsequence)
@@ -250,17 +250,19 @@ get_param_ranges <- function(all_params, fam) {
     accepted_float <- sum(!is_integer & testoutcome)
     accepted_float_rate <- accepted_float/(num_tests - num_integer)
 
-    if(accepted_float_rate > (1/num_tests - num_integer)) accepts_float_param = TRUE
+    if(accepted_float_rate > 1/(num_tests - num_integer)) accepts_float_param <- TRUE
     else if(accepted_int_rate == 0) error("distribution does not seem to accept any values")
-    else accepts_float_param = FALSE
+    else accepts_float_param <- FALSE
 
     accepts_float[param] <- accepts_float_param
   }
   return(list(lower=lower,
               upper=upper,
-	      accepts_float=accepts_float))
+              accepts_float=accepts_float))
 }
 
+# example:
+get_param_ranges(all_params, fam)
 
 
 ### Final function -------------------------------------------------------------------------------------------------------
@@ -289,6 +291,8 @@ for (fam in families) {
   print(ranges$lower)
   cat("Upper bounds:\n")
   print(ranges$upper)
+  cat("Accepts floats:\n")
+  print(ranges$accepts_float)
 }
 
 ### TODO:
