b7df4507295d72e27d74023b990735d74b43092b
leolino97 <32536315+leolino97@users.noreply.github.com>
2019-10-28 19:44:47 +0100

    second commit

--- loglik.R
+++ loglik.R
@@ -1,29 +1,49 @@
 
+# rm(list=ls())
+
+# args: benamte Liste
+# fixed: benamte Liste
 
 # Loglik-Function
-loglik <- function(param_values, family, data, fixed=list(), log=T) {
+loglik <- function(param_values, family, data, fixed, log) {
   arguments <- list(x=data)
   # check wheter log-distribution function is directly available for distribution
   if(log==T)
     arguments$log <- T
-  # add variable parameter values of distribution to list
-  if(length(param_values)>0) {
-    arguments <- c(arguments, param_values)
-  }
   # add fixed parameter values of distribution to list
   if(length(fixed)>0) {
     arguments <- c(arguments, fixed)
   }
-  # calculate vector of (log)-densities
-  summands <- do.call(paste('d', family, sep=''), args=arguments)
-  if(any(is.na(summands)))
-    stop('In Log-Likelihood-Function NA occured.')
-  # log values if not log so far
-  if(!log) {
-    summands <- log(summands)
-    warning('Could be numerically instable.')
-  } 
-  ll <- sum(summands)
-  
+  # define loglikelihood function 
+  likelihood <- function(params) {
+    if(length(params)>0) {
+      arguments <- c(arguments, params)
+    }
+    else{warning('loglik does not depend on parameters.')}
+    summands <- do.call(paste('d', family, sep=''), args=arguments)
+    if(any(is.na(summands)))
+      stop('In Log-Likelihood-Function NA occured.')
+    # log values if not log so far
+    if(!log) {
+      summands <- log(summands)
+      warning('Could be numerically instable.')
+    } 
+    loglik_value <- sum(summands)
+    return(loglik_value)
+  }
+  return(likelihood)
 }
+
+
+
+likelihood <- loglik(param_values = list(), family = "unif", data = 1:10, fixed=list(min=1, max=10), log=T)
+
+likelihood <- loglik(param_values = list(), family = "unif", data = 1:10, fixed=list(min=1, max=10), log=F)
+
+likelihood <- loglik(param_values = list(), family = "exp", data = 1:10, fixed=list(), log=T)
+
+likelihood <- loglik(param_values = list(), family = "norm", data = 1:10, fixed=list(), log=T)
+
+
+
 
