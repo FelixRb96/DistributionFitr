c180851f6579a7056afb95d5766f9e88921f7499
Kiril Dik <kiril.dik@web.de>
2019-11-18 17:41:12 +0100

    doc: added citation file

---
+++ inst/CITATION.R
@@ -0,0 +1,28 @@
+year <- sub("-.*", "", meta$Date)
+
+note <- sprintf("R package version %s", meta$Version) 
+
+bibentry(bibtype = "Manual",          
+         title = "{DistributionFitr}: Fitting Multiple Distributions",
+         author = c(person("Benedikt", "Geier", role="aut"),  
+                    person("Moritz", "Kern", role="aut"),  
+                    person(given=c("Borui","Niklas"), "Zhu", role="aut"), 
+                    person("Kiril", "Dik", role="aut"),
+                    person("André", "Ferdinand", role="aut"),                     
+                    person("Till", "Freihaut", role="aut"), 
+                    person("Tim", "Glockner", role="aut"),   
+                    person(given("Manuel","J."), "Hentschel", role="aut"),
+                    person("Adrian", "Heppeler", role="aut"),     
+                    person("Moritz", "Lauff", role="aut"),                       
+                    person("Niclas", "Lietzow", role="aut"),                     
+                    person("Hélène", "Peter", role="aut"),
+                    person("Nadine", "Tampe", role="aut"),                     
+                    person(given=c("Valentin","v."), "Trotha", role="aut"),
+                    person("Leonardo", "Vela", role="aut"),
+                    person("Martin", "Schlather", role="cre"),                     
+                    person("Yiqi", "Li", role="ctb"),
+                    person("Henrik", "Bengtsson", role="ctb"),
+                    year = year,          
+                    note = note,          
+                    url = "http://CRAN.R-project.org/package=DistributionFitR")
+                    )
