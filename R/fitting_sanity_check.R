93d046ca76a67b68f5de74a7cac4ebce6d806857
moritzkern <moritz.kern@stads.de>
2019-11-23 18:20:12 +0100

    feat: better sanity check, useful results with globalfit can be obtained

--- R/fitting_sanity_check.R
+++ R/fitting_sanity_check.R
@@ -8,9 +8,9 @@ fitting_sanity_check <- function(object, data, continuity, plot=F) {
   lower <- min(data) - 0.2 * (max(data)-min(data))
   upper <- max(data) + 0.2 * (max(data)-min(data))
   
-  breaks <- ifelse(continuity, sqrt(length(data)), min(nclass.Sturges(x@data), length(unique(x@data))))
+  breaks <- ifelse(continuity, sqrt(length(data)), min(nclass.Sturges(data), length(unique(data))))
   h <- suppressWarnings(hist(x = data, xlim=range(lower,upper), freq = FALSE, xlab = 'x', ylab = 'density', breaks=breaks, plot=plot))
-  
+
   x <- h$mids
   y <- h$density
   command <- paste0("get_fun_from_package(fam = '", object@family, "', '", object@package, "', 'd')(x, ",
@@ -18,11 +18,16 @@ fitting_sanity_check <- function(object, data, continuity, plot=F) {
   z <- eval(parse(text = command))
   if(plot)
     lines(x,z, col='red')
-  meanquot <- sum(x)/sum(y)
+
+  # meanquot <- sum(x)/sum(y)
+  # good <- meanquot > 0.01 & meanquot<100
   
-  good <- meanquot > 0.01 & meanquot<100
+  meanquot <- sum(diff(h$breaks) * z)
+  good <- meanquot > 0.5 & meanquot < 2
+  
   return(list(meanquot=meanquot, good=good))
 }
 
 
-# fitting_sanity_check(r@fits[[30]], rnorm(n = 1000, mean=10, sd=1), continuity = T, plot = T)
+#fitting_sanity_check(r@fits[[10]], rnorm(n = 1000, mean=10, sd=10), continuity = T, plot = T)
+
--- private/test_globalfit.R
+++ private/test_globalfit.R
@@ -1,12 +1,30 @@
-r <- globalfit(rnorm(n = 1000, mean=10, sd=1))
-summary(r, count=100, which=1)  
+#####
+# Solver for current issue, need to be deleted, after it is solved
+tempwrapper <- function(r) {
+  i <- 1
+  while(i<=length(r@fits)) {
+    if(class(r@fits[[i]])[1]!= 'optimParams') {
+      r@fits[[i]] <- NULL
+    } else {
+      i <- i+1
+    }
+  }
+  return(r)
+}
+######
+
+
+
+r <- tempwrapper(globalfit(rnorm(n = 1000, mean=10, sd=1)))
+
+summary(r, count=10, which=2)  
 hist(r)
-for(i in 1:10) {
+for(i in 1:20) {
   tryCatch(hist(r, which=i), error=function(x) return(NA))
   Sys.sleep(1)
 }
 
-r <- globalfit(rgamma(n = 10000, shape=3, rate = 4))
+r <- tempwrapper(globalfit(rgamma(n = 10000, shape=3, rate = 4)))
 summary(r, ic='BIC')
 summary(r, ic='AICc')
 summary(r, which=1:3)
@@ -18,7 +36,7 @@ hist(r, ic='BIC', which=18)
 hist(r, ic='BIC', which=4)
 hist(r, ic='BIC', which=30)
 
-r <- globalfit(rbinom(n = 10000, size=10, prob=0.7))
+r <- tempwrapper(globalfit(rbinom(n = 1000, size=10, prob=0.7)))
 summary(r)
 
 AIC(r, count=2)
@@ -27,6 +45,8 @@ BIC(r)
 hist(r)
 
 
+
+# examples, tests, 
 x <- seq(6,20, 0.01)
 y <- seq(0,10,0.1)
 plot(x, CoSMoS::dggamma(x=x, scale=3.59, shape1=19.09, shape2=5.46))
@@ -34,8 +54,6 @@ plot(x, CoSMoS::dggamma(x=x, scale=4, shape1=20, shape2=5.46))
 plot(y, CoSMoS::dggamma(x=y, scale=1, shape1=1, shape2=1))
 
 
-#Density  d
-# coummulatiive p
 x <- 0:10 
 plot(ForestFit::dgsm(x, omega = 5.491870e+08, beta = 6.732193e-02 ))
 
