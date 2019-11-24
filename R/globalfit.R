040bf93205da0260c15bed10c955e97c6e17f5cd
benediktgeier <benedikt1geier@web.de>
2019-11-24 13:23:02 +0100

    feat: Added copyright headers

--- R/globalfit.R
+++ R/globalfit.R
@@ -1,3 +1,28 @@
+## Authors 
+## Moritz Lauff, mlauff@mail.uni-mannheim.de
+## Kiril Dik, kiril.dik@web.de
+## Nadine Tampe
+##
+## Fit multiple distribution families to a given univariate dataset
+##
+## Copyright (C) 2019 -- 2020 Moritz Lauff
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 3
+## of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  
+
+
+
 
 ### 1)  benoetigte Hilfsfunktionen -------------------------------------------------------------------------
 
--- R/optimParamsContinuous.R
+++ R/optimParamsContinuous.R
@@ -1,3 +1,27 @@
+## Authors 
+## Moritz Kern, mkern@mail.uni-mannheim.de
+## Benedikt Geier, bgeier@mail.uni-mannheim.de
+##
+## Optimise the continuous parameters of a distribution
+##
+## Copyright (C) 2019 -- 2020 Moritz Kern
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 3
+## of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+
+
+
 # helper function
 get_best_result_from_progress <- function(optim_progress) {
   best_idx <- which.max(optim_progress$log_lik)
