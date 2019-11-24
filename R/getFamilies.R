f30036eb7fc834ec2419884c841ab76b9cce9308
timlgl <tim.glockner@outlook.com>
2019-11-24 22:23:22 +0100

    comments, replaced cat

--- R/getFamilies.R
+++ R/getFamilies.R
@@ -21,27 +21,27 @@
 ## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  
 
 
-#iterate over packages and extract families and params
+##iterate over packages and extract families and params
 iterate_packages <- function(packages) {
   
   res <- list()
   
-  #iterate over packages
+  ##iterate over packages
   for (i in 1:length(packages)) {
     ## package <- packages[i]
-    cat("Current Package:", packages[i])
+    message("Current Package:", packages[i])
     package_content <- getFamily(packages[i])
-    cat("\tNumber of families:", length(package_content), "\n")
+    message("\tNumber of families:", length(package_content), "\n")
     
-    # no distribution family contained in package
+    ## no distribution family contained in package
     if (length(package_content) == 0) next
     
-    #iterate over all families withing package
+    ##iterate over all families withing package
     for (j in 1:length(package_content)) {
-      #
+      
       stopifnot(package_content[[j]]$package == packages[i])
       
-      cat("Current Family:", package_content[[j]]$family, "\n")
+      message("Current Family:", package_content[[j]]$family, "\n")
       #fetch params for each family and add to 
       params <- tryCatch(getParams(package_content[[j]]),
                          error = function(e) {
@@ -51,7 +51,7 @@ iterate_packages <- function(packages) {
                            ## message(e, "\n")
                            NULL
                          })
-      if (!is.null(params)) ## length(params) != 0 ## semantisch das richtige?
+      if (!is.null(params)) # length(params) != 0 ## semantisch das richtige?
         res[[length(res) + 1]] <- c(package_content[[j]],
                                     list(family_info = params))
     }
@@ -69,10 +69,9 @@ construct_package_list <- function(all.packages) {
     all.packages <- as.vector(installed.packages()[,"Package"])
     
   } else {
-    #only select "base" and "recommended" packages
+    ##only select "base" and "recommended" packages
     ins_pck <- installed.packages()
     
-    ## boolean value of what to keep
     package_filter <- ((ins_pck[,"Priority"] == "base" |
                         ins_pck[,"Priority"] == "recommended")
       & !is.na(ins_pck[,"Priority"]))
@@ -88,34 +87,41 @@ construct_package_list <- function(all.packages) {
 
 write_file <- function(family_list, file="data/all_families.rds") {
   saveRDS(family_list, file=file)
+  #when bottleneck solves:
+  #con <- file("all_families.R")
+  #open(con, "w")
+  #write("FamilyList<-", con)
+  #dput(x=FamilyList, file=con)
+  #close(con)
+  
 }
 
 
-# Case 1: all.packages gegeben: Suche nach Verteilungsfamilien
-# Case 1.1 vector of strings (Liste von Packetnamen) -> take families from those packages
-# Case 1.2 FALSE -> only take recommended / base packages
-# Case 1.3 TRUE -> take all installed packages
-# Case 2 all.packages missing: Take families saved in the file
+### Case 1: all.packages gegeben: Suche nach Verteilungsfamilien
+### Case 1.1 vector of strings (Liste von Packetnamen) -> take families from those packages
+### Case 1.2 FALSE -> only take recommended / base packages
+### Case 1.3 TRUE -> take all installed packages
+### Case 2 all.packages missing: Take families saved in the file
 
 ## MS: all_families.rds darf nicht in R sein. Habe es nach privat verschoben
 getFamilies <- function(all.packages, file="private/all_families.rds") {
-  # CASE 2:
+  ## CASE 2:
   if (missing(all.packages)) {
     if (!file.exists(file))
       return(getFamilies(all.packages = FALSE, file=file))
-    #read file and return list of lists
+    ##read file and return list of lists
     family_list <- readRDS(file=file)
     return(family_list)
   }
 
-  # CASE 1.2 & 1.3
+  ## CASE 1.2 & 1.3
   if (is.logical(all.packages)) {
     family_list <- iterate_packages(construct_package_list(all.packages = all.packages))
     write_file(family_list=family_list,file=file)
     return(family_list)
   }
   
-  # CASE 1.1
+  ## CASE 1.1
   family_list <- iterate_packages(all.packages)
   write_file(family_list=family_list,file = file)
   return(family_list)
